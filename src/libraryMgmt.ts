57ac51b67b908ee05057beb5b7e2feab19a43f53
standsi <stan@padgettholdings.com>
2025-02-22 22:04:46 -0500

    add configs for libs and bring in private zip and download methods

--- src/libraryMgmt.ts
+++ src/libraryMgmt.ts
@@ -27,7 +27,131 @@ export class LibraryMgmt {
 
     private _statusBarItem: vscode.StatusBarItem;
 
+    // ** public methods **
+
+
+
+    // ** private methods **
+
+    private async downloadOrigBundle(libTag: string, pyLibFmt: string): Promise<string> {
+        try {
+            const response = await axios.default({
+                method: 'get',
+                url: `https://github.com/adafruit/Adafruit_CircuitPython_Bundle/releases/download/${libTag}/adafruit-circuitpython-bundle-${pyLibFmt}-${libTag}.zip`,
+                responseType: 'stream'
+            });
     
+            response.data.pipe(fs.createWriteStream(`adafruit-circuitpython-bundle-${pyLibFmt}-${libTag}.zip`));
+    
+            return new Promise((resolve, reject) => {
+                response.data.on('end', () => {
+                    resolve('File downloaded successfully for fmt:' + pyLibFmt);
+                });
+    
+                response.data.on('error', (err: any) => {
+                    reject(err);
+                });
+            });
+        } catch (error) {
+            console.error('Error downloading the file:', error);
+            throw error;
+        }
+    }
+    
+    private async downloadLibMetadata(libTag: string): Promise<string> {
+        try {
+            const response = await axios.default({
+                method: 'get',
+                url: `https://github.com/adafruit/Adafruit_CircuitPython_Bundle/releases/download/${libTag}/adafruit-circuitpython-bundle-${libTag}.json`,
+                responseType: 'json'
+            }).then((response) => {
+                const dest = `/home/stan/my-typescript-project/ziplibextract/adafruit-circuitpython-bundle-${libTag}.json`;
+                fs.writeFileSync(dest, JSON.stringify(response.data), {
+                    encoding: "utf8",
+                });
+                console.log('Downloaded lib metadata to:', dest);
+            });
+        } catch (error) {
+            console.error('Error downloading the file:', error);
+            throw error;
+        }
+        return 'done';
+    }
+    
+    private async getLatestBundleTag(): Promise<string> {
+        let r: axios.AxiosResponse = await axios.default.get(
+            "https://github.com/adafruit/Adafruit_CircuitPython_Bundle/releases/latest",
+            { headers: { Accept: "application/json" } }
+        );
+        return await r.data.tag_name;
+    }
+    
+    
+    private async getOrigBundle(libTag: string, pyLibFmt: string): Promise<string> {
+        //    try {
+        const res = await this.downloadOrigBundle(libTag, pyLibFmt);
+        console.log('Downloaded orig bundle for pylibfmt: ' + pyLibFmt + ' with result:', res);
+        //    } catch (error) {
+        //        console.error(error);
+        //    }
+        return 'done';
+    }
+    
+    private async ziplibextract(libOnlyZipSource: string, libOnlyZipTempDir: string) {
+        const unzip = new zl.Unzip({
+            overwrite: true,
+            // Called before an item is extracted.
+            onEntry: function (event) {
+                if (/\/examples\//.test(event.entryName)) {
+                    event.preventDefault();
+                } else if (/\/requirements\//.test(event.entryName)) {
+                    event.preventDefault();
+                } else {
+                    //console.log(event.entryCount, event.entryName);
+                }
+            }
+        });
+        //await unzip.extract("/home/stan/my-typescript-project/adafruit-circuitpython-bundle-py-20241221.zip", "/home/stan/my-typescript-project/ziplibextract/");
+        await unzip.extract(libOnlyZipSource, libOnlyZipTempDir);
+        // .then(function () {
+        //     console.log("done");
+        // }, function (err) {
+        //     console.log(err);
+        // });
+    }
+    
+    private async ziplibzip(libOnlyZipTempDir: string, libOnlyZipArchiveName: string, libOnlyZipFile: string) {
+        //await zl.archiveFolder("/home/stan/my-typescript-project/ziplibextract/adafruit-circuitpython-bundle-py-20241221/lib/", "/home/stan/my-typescript-project/ziplibextract/adafruit-circuitpython-bundle-py-20241221-lib.zip");
+        //just zip up the lib folder in the temp
+        await zl.archiveFolder(libOnlyZipTempDir + libOnlyZipArchiveName + "/lib/", libOnlyZipFile);
+    }
+    
+    private async ziplibextractneeds(libNeeds: string[], libOnlyZipFile: string, libExtractTarget: string) {
+        const unzip = new zl.Unzip({
+            overwrite: true,
+            // Called before an item is extracted.
+            onEntry: function (event) {
+                if (!libNeeds.some((libName) => {
+                    const rg = new RegExp(`^${libName}`);
+                    return rg.test(event.entryName);
+                }))    ///\/examples\//.test(event.entryName))
+                {
+                    event.preventDefault();
+                } else {
+                    //console.log(event.entryCount, event.entryName);
+                }
+            }
+        });
+        //await unzip.extract("/home/stan/my-typescript-project/ziplibextract/adafruit-circuitpython-bundle-py-20241221-lib.zip", "/home/stan/my-typescript-project/ziplibextract/libstubs/");
+        await unzip.extract(libOnlyZipFile, libExtractTarget);
+        // .then(function () {
+        //     console.log("done");
+        // }, function (err) {
+        //     console.log(err);
+        // });
+    }
+    
+
 
 
 }
