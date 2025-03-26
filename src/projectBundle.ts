e017094d85e97e17dc4ba00c7a25a08a2fadc4d0
standsi <stan@padgettholdings.com>
2025-03-26 17:15:13 -0400

    Complete #79 - add local file pick option in addition to URL for project bundle.  Also writes any readme files in the root of project.

--- src/projectBundle.ts
+++ src/projectBundle.ts
@@ -51,16 +51,24 @@ export class ProjectBundleMgmt {
                 prompt: strgs.enterProjectBundleUrl,
                 placeHolder: strgs.projectBundleUrlPlaceholder,
                 validateInput: (input) => {
-                    if (!input || !input.toLowerCase().startsWith('https')) {
+                    //if (!input || !input.toLowerCase().startsWith('https')) {
+                    // ** #79 allow just enter to go into file pick
+                    if(!(input && input.toLowerCase().startsWith('https')) && !(input==='')){
                         return strgs.enterProjectBundleUrl;
                     }
                     return null;
                 },
             });
-            if (!projectBundleUrl) {
+            // #79, if just enter, then use file picker, else undefined exits
+            if (projectBundleUrl===undefined) {
                 return;
             }
-            // try to download the bundle to a temp directory
+            // flag to indicate if we need to use file picker
+            let useFilePicker=false;
+            if (projectBundleUrl==='') {
+                useFilePicker=true;
+            }
+            // either the downloaded bundle or local file needs to go in to a temp directory
             if(!this._projectBundleArchiveUri){
                 vscode.window.showErrorMessage(strgs.projectBundleArchiveDirNotInitErr);
                 return;
@@ -69,29 +77,63 @@ export class ProjectBundleMgmt {
                     fs.mkdirSync(this._projectBundleArchiveUri.fsPath);
                 }
             }
-            // try to get the id from the url, if not use the date
-            // example:
-            // https://learn.adafruit.com/elements/3192127/download?type=zip
-            const pat=/elements\/(\d+)\/download/;
-            const match=projectBundleUrl.toLowerCase().match(pat);
-            const id=match?match[1]:new Date().toISOString().split('T')[0].replace(/[^0-9]/g,'');
-            const projectBundleArchiveFile = path.join(this._projectBundleArchiveUri.fsPath, `projectBundle${id}.zip`);
-            // check to see if file already exists, offer to skip re-download
-            let skipDownload=false;
-            if(fs.existsSync(projectBundleArchiveFile)){
-                const ans=await vscode.window.showWarningMessage(strgs.projectBundleDnldExistsSkipQues,'Yes','No, download again');
-                if(ans==='Yes'){
-                    skipDownload=true;
+            // will need to track the filename since it will be different for url vs picked
+            let projBundleZipFilename:string='';
+            let projectBundleZipUri:vscode.Uri;
+            let projectBundleArchiveFile:string='';     // this is the eventual fspath to arch zip file
+            // if picking file try to get it with the file open; if cancel then bail
+            if(useFilePicker){
+                const projectBundleZipUriPick = await vscode.window.showOpenDialog({
+                    canSelectMany: false,
+                    canSelectFolders: false,
+                    filters: {
+                        'Zip Files': ['zip']
+                    },
+                    defaultUri: vscode.Uri.file(os.homedir())
+                });
+                if (!projectBundleZipUriPick || projectBundleZipUriPick.length===0) {
+                    return;
                 }
-            }
-            if (!skipDownload) {
+                // set the url to the picked file
+                projectBundleZipUri=projectBundleZipUriPick[0];
+                projBundleZipFilename=path.basename(projectBundleZipUri.fsPath);
+                // copy to the projectBundleArchiveUri so we can unzip it
+                const projectBundleArchiveFileUri = vscode.Uri.joinPath(this._projectBundleArchiveUri, projBundleZipFilename);
                 try {
-                    await this.downloadFile(projectBundleUrl, projectBundleArchiveFile);
+                    await vscode.workspace.fs.copy(projectBundleZipUri, projectBundleArchiveFileUri, {overwrite:true});
                 } catch (error) {
                     vscode.window.showErrorMessage(this.getErrorMessage(error));
                     return;
                 }
+                projectBundleArchiveFile=projectBundleArchiveFileUri.fsPath;  // this is the fspath to the archive zip file
+            } else {
+                // doing a url...
+                // try to get the id from the url, if not use the date
+                // example:
+                // https://learn.adafruit.com/elements/3192127/download?type=zip
+                const pat=/elements\/(\d+)\/download/;
+                const match=projectBundleUrl.toLowerCase().match(pat);
+                const id=match?match[1]:new Date().toISOString().split('T')[0].replace(/[^0-9]/g,'');
+                projBundleZipFilename=`projectBundle${id}.zip`;
+                projectBundleArchiveFile = path.join(this._projectBundleArchiveUri.fsPath, projBundleZipFilename);
+                // check to see if file already exists, offer to skip re-download
+                let skipDownload=false;
+                if(fs.existsSync(projectBundleArchiveFile)){
+                    const ans=await vscode.window.showWarningMessage(strgs.projectBundleDnldExistsSkipQues,'Yes','No, download again');
+                    if(ans==='Yes'){
+                        skipDownload=true;
+                    }
+                }
+                if (!skipDownload) {
+                    try {
+                        await this.downloadFile(projectBundleUrl, projectBundleArchiveFile);
+                    } catch (error) {
+                        vscode.window.showErrorMessage(this.getErrorMessage(error));
+                        return;
+                    }
+                }
             }
+            // now we have the project bundle zip file in the archive directory
             //now extract into temp directory
             if(!this._projectBundleTempUri){
                 vscode.window.showErrorMessage(strgs.projectBundleTempDirNotInitErr);
@@ -101,7 +143,7 @@ export class ProjectBundleMgmt {
                     fs.mkdirSync(this._projectBundleTempUri.fsPath);
                 }        
             }
-            const projectBundleTempDirUri = vscode.Uri.joinPath(this._projectBundleTempUri, `projectBundle${id}`);
+            const projectBundleTempDirUri = vscode.Uri.joinPath(this._projectBundleTempUri, projBundleZipFilename.replace(/\.zip$/,''));
             try {
                 const unzip=new zl.Unzip({
                     overwrite: true
@@ -114,19 +156,28 @@ export class ProjectBundleMgmt {
             }
             // now read down through the extracted files/folders, looking for circuitpython N.x
             let foundCPy=false;
+            let curProjBundleTempDirFinalUri:vscode.Uri=vscode.Uri.file('');   // final circuitpython dir
+            // keep array of readme files if found down through dirs so can copy to workspace
+            let readmeFiles:string[]=[];
             let curProjBundleTempDirUri=projectBundleTempDirUri;
             while(!foundCPy){
                 let lastProjBundleTempDirUri=curProjBundleTempDirUri;
                 const tmpDirContents=await vscode.workspace.fs.readDirectory(curProjBundleTempDirUri);
                 for(const [name,type] of tmpDirContents){
-                    if(type===vscode.FileType.Directory && name.toLowerCase().startsWith('circuitpython')){
-                        curProjBundleTempDirUri=vscode.Uri.joinPath(curProjBundleTempDirUri,name);
+                    // check for readme 
+                    if(type===vscode.FileType.File && name.toLowerCase().startsWith('readme')){
+                        // keep track of any readme files found so can copy to workspace
+                        readmeFiles.push(vscode.Uri.joinPath(curProjBundleTempDirUri,name).fsPath);
+                    }
+                    if(type===vscode.FileType.Directory &&  /circuitpython\s+[0-9](?:\.x)?/i.test(name.toLowerCase()  )){       //startsWith('circuitpython')){
+                        curProjBundleTempDirFinalUri=vscode.Uri.joinPath(curProjBundleTempDirUri,name);
                         foundCPy=true;
-                        break;
+                        //break;
                     }
-                    if(type===vscode.FileType.Directory){
-                        lastProjBundleTempDirUri=vscode.Uri.joinPath(curProjBundleTempDirUri,name);
-                    }
+                        if(type===vscode.FileType.Directory){
+                            lastProjBundleTempDirUri=vscode.Uri.joinPath(curProjBundleTempDirUri,name);
+                        }
+                    
                 }
                 // if did not find cp dir, and did not change the directory, then no more dirs to go down
                 if(foundCPy || curProjBundleTempDirUri.fsPath===lastProjBundleTempDirUri.fsPath){
@@ -143,9 +194,10 @@ export class ProjectBundleMgmt {
             }
             //now read the cp directory and copy all files and folders to the workspace
             //####TBD#### ask if can overwrite
-            const projBundleContents=await vscode.workspace.fs.readDirectory(curProjBundleTempDirUri);
+            const projBundleContents=await vscode.workspace.fs.readDirectory(curProjBundleTempDirFinalUri);
             // check to see if any of the files or folders from bundle will
             //   overwrite in the workspace
+            // ** don't worry about readme files, will copy first then can be overwritten
             const wscontents=await vscode.workspace.fs.readDirectory(this._workspaceUri);
             for(const [name,type] of projBundleContents){
                 if(wscontents.some(fle => fle[0].toLowerCase()===name.toLowerCase())){
@@ -158,9 +210,15 @@ export class ProjectBundleMgmt {
                     }
                 }
             }
-
+            // copy any readme files found to the workspace
+            for(const readmeFile of readmeFiles){
+                const readmeFileUri=vscode.Uri.file(readmeFile);
+                const readmeDestUri=vscode.Uri.joinPath(this._workspaceUri,path.basename(readmeFile));
+                await vscode.workspace.fs.copy(readmeFileUri, readmeDestUri, {overwrite:true});
+            }
+            // now copy the contents of the project bundle to the workspace
             for(const [name,type] of projBundleContents){
-                const flfldrTocopy=vscode.Uri.joinPath(curProjBundleTempDirUri,name);
+                const flfldrTocopy=vscode.Uri.joinPath(curProjBundleTempDirFinalUri,name);
                 const flfldrDest=vscode.Uri.joinPath(this._workspaceUri,name);
                 if(type===vscode.FileType.Directory){
                     await vscode.workspace.fs.copy(flfldrTocopy,flfldrDest,{overwrite:true});
